c8e7a24e2b766bb6c1a2978c2ce20486781f0065
khombole vibes <khombolevibes@gmail.com>
2025-07-24 15:36:12 +0000

    The NextJS app server is having trouble starting. Please identify what c

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,6 @@
 import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
-  output: 'export', // <= THIS IS THE IMPORTANT CHANGE
   /* config options here */
   typescript: {
     ignoreBuildErrors: true,
